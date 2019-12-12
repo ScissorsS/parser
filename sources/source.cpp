97acf63f9dd7854ffc73771e87debe90511da295
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2019-12-12 20:44:53 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,43 @@
 // Copyright 2018 Your Name <your_email>
 
-#include <header.hpp>
+#ifndef INCLUDE_HEADER_HPP_
+#define INCLUDE_HEADER_HPP_
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <any>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Json {
+public:
+
+Json(const std::string& s): str(s)
+
+bool is_array() const
+
+bool is_object() const
+
+std::any& operator[](const std::string& key)
+
+std::any& operator[](int index)
+
+private:
+string findKey(const string& s)
+
+any findMean(const string& s)
+
+public:
+static Json parse(const std::string& s)
+
+static Json parseFile(const std::string& path_to_file)
+
+int position;
+std::string str;
+std::map <std::string, std::any> parsed;
+
+}
+#endif // INCLUDE_HEADER_HPP_
